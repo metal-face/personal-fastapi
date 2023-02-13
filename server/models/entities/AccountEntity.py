00fe85bb118c4798b514aaae6a415171736941e4
metal-face <capatain@pirateradio.boats>
2023-02-12 22:03:38 -0500

    User account entity written

---
+++ server/models/entities/AccountEntity.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class Account(BaseModel):
+    id: UUID
+    username: str
+    email: str
+    password: str
+    created_at: datetime 
