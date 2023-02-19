aaa0de6ac58d8fddbabb8947273b3b2a364eca44
metal-face <capatain@pirateradio.boats>
2023-02-18 23:06:44 -0500

    create account route functional

--- server/repositories/accounts.py
+++ server/repositories/accounts.py
@@ -1,19 +1,21 @@
-from fastapi import Request
+from typing import Any
+
+from server import services
+
+READ_PARAMS = "id, username, email, created_at"
 
 
-async def create_account(username: str, email: str, password: str, request: Request):
-    create_account_statement = "INSERT INTO accounts (username, email, password) VALUES (:username, :email, :password)"
-
-    create_params = {"username": username, "email": email, "password": password}
-
-    row_id_returned = request.app.state.database.execute(
-        create_account_statement, create_params
+async def create_account(username: str, email: str, password: str) -> dict[str, Any]:
+    account = await services.database.execute(
+        query=f"""
+            INSERT INTO accounts (username, email, password)
+            VALUES (:username, :email, :password)
+            RETURNING ({READ_PARAMS})
+        """,
+        values={
+            "username": username,
+            "email": email,
+            "password": password,
+        },
     )
-
-    account_info_params = {"id": row_id_returned}
-
-    account_info = (
-        "SELECT id, username, email, password, created_at FROM accounts WHERE :id"
-    )
-
-    return request.app.state.database.fetch_one(account_info, account_info_params)
+    return account
