7916f724143be880e6071108262001755d70d5f1
metal-face <hughesbryan3000@gmail.com>
2023-02-19 01:46:32 -0500

    Accounts controller built out
    Fetch by id, fetch all, and create written

--- server/repositories/accounts.py
+++ server/repositories/accounts.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, Union
+from uuid import UUID
 
 from server import services
 
@@ -6,7 +7,7 @@ READ_PARAMS = "id, username, email, created_at"
 
 
 async def create_account(username: str, email: str, password: str) -> dict[str, Any]:
-    account = await services.database.execute(
+    account = await services.database.fetch_one(
         query=f"""
             INSERT INTO accounts (username, email, password)
             VALUES (:username, :email, :password)
@@ -18,4 +19,29 @@ async def create_account(username: str, email: str, password: str) -> dict[str, 
             "password": password,
         },
     )
-    return account
+    assert account is not None
+    return dict(account._mapping)
+
+
+async def get_all_accounts() -> list[dict[str, Any]]:
+    accounts = await services.database.fetch_all(
+        query=f"""
+            SELECT {READ_PARAMS} 
+            FROM accounts
+        """,
+    )
+    return [dict(account) for account in accounts]
+
+
+async def get_account_by_id(id: UUID) -> Union[dict[str, Any], None]:
+    account = await services.database.fetch_one(
+        query=f"""
+            SELECT {READ_PARAMS} 
+            FROM accounts 
+            WHERE id = :id
+        """,
+        values={
+            "id": id,
+        },
+    )
+    return dict(account) if account is not None else None
