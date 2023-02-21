051e8e5dc1656e4d98f5e6745b3d32e63b4fbbbd
metal-face <captain@pirateradio.boats>
2023-02-21 00:57:38 -0500

    Session entity defined

---
+++ server/models/entities/SessionEntity.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Session(BaseModel):
+    session_id: UUID
+    account_id: int
+    user_agent: str
+    expires_at: datetime
+    created_at: datetime
+    updated_at: datetime
