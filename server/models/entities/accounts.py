6d8c718f23faaf8daba109e6de80b6d874b8391a
metal-face <captain@pirateradio.boats>
2023-02-25 17:56:32 -0500

    files renamed for continuity

---
+++ server/models/entities/accounts.py
@@ -0,0 +1,10 @@
+from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class Account(BaseModel):
+    id: UUID
+    username: str
+    email: str
+    created_at: datetime 
