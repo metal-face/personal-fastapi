74eabe63df59faae788ac27996f2e73dd6a34ff5
metal-face <hughesbryan3000@gmail.com>
2023-02-25 17:48:42 -0500

    buisness logic written for accounts
    Create, GetByID, GetMany, UpdateByID, and DeleteById written

--- server/logic/accounts.py
+++ server/logic/accounts.py
@@ -1,4 +1,6 @@
-import typing
+from typing import Any, Union
+from uuid import UUID
+
 
 from server.utils.errors import ServiceError
 import server.utils.validation as validation
@@ -10,7 +12,7 @@ async def signup(
     email_address: str,
     password: str,
     username: str,
-) -> dict[str, typing.Any] | ServiceError:
+) -> Union[dict[str, Any], ServiceError]:
     if not validation.validate_username(username):
         return ServiceError.ACCOUNTS_USERNAME_INVALID
 
@@ -22,8 +24,53 @@ async def signup(
 
     if await accounts.get_account_by_email(email=email_address) is not None:
         return ServiceError.ACCOUNTS_EMAIL_ADDRESS_EXISTS
-    
+
     if await accounts.get_account_by_username(username=username) is not None:
         return ServiceError.ACCOUNTS_USERNAME_EXISTS
 
+    account = await accounts.create_account(
+        email=email_address,
+        password=password,
+        username=username,
+    )
+
+    if account is None:
+        return ServiceError.ACCOUNTS_SIGNUP_FAILED
+
+    return account
+
+
+async def get_account_by_id(id: UUID) -> Union[dict[str, Any], ServiceError]:
+    account = await accounts.get_account_by_id(id)
+
+    if account is None:
+        return ServiceError.ACCOUNTS_NOT_FOUND
+
+    return account
+
+
+async def get_account_by_email(email: str) -> Union[dict[str, Any], ServiceError]:
+    account = await accounts.get_account_by_email(email)
+
+    if account is None:
+        return ServiceError.ACCOUNTS_NOT_FOUND
+
+    return account
+
+
+async def get_account_by_username(username: str) -> Union[dict[str, Any], ServiceError]:
+    account = await accounts.get_account_by_username(username)
+
+    if account is None:
+        return ServiceError.ACCOUNTS_NOT_FOUND
+
+    return account
+
+
+async def fetch_many_accounts(page: int, page_size: int) -> Union[list[dict[str, Any]], ServiceError]:
+    user_accounts = await accounts.get_many_accounts(page, page_size)
     
+    if user_accounts is None:
+        return ServiceError.ACCOUNTS_NOT_FOUND
+    
+    return user_accounts
