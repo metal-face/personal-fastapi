66386c0039ab669abe721aeb7a89daddb541eacb
metal-face <hughesbryan3000@gmail.com>
2023-04-01 21:36:16 -0400

    fixes

--- mount/server/utils/settings.py
+++ mount/server/utils/settings.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 APP_COMPONENT = os.environ["APP_COMPONENT"]
+APP_HOST = os.environ["APP_HOST"]
+APP_PORT = int(os.environ["APP_PORT"])
 APP_ENV = os.environ["APP_ENV"]
 
 DB_DRIVER = os.environ["DB_DRIVER"]
@@ -13,7 +15,6 @@ DB_PASS = os.environ["DB_PASS"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = int(os.environ["DB_PORT"])
 DB_NAME = os.environ["DB_NAME"]
-
 
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = int(os.environ["REDIS_PORT"])
