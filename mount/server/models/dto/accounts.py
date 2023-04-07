d5916152aa8b65a006d030b8e0d95a3e2367dc20
metal-face <hughesbryan3000@gmail.com>
2023-04-07 15:02:57 -0400

    ENUM support added to accounts model.
    In order to support the role system around a user, an ENUM was
    used to define which values are acceptable as a role.
    
    A user, when created, is REGULAR by default. Regular allows you
    to view other user's content, but it does not allow you to
    perform CRUD operations at all.
    
    Right now, the only user with ADMIN privledges is myself. This
    will allow me to grant SUPER privledges to others, as well as
    perform CRUD operations on all users content.
    
    When a user is a SUPER user, this will allow them to perform
    CRUD operations on their own content, but this will not allow
    them to peform CRUD operations on any other user's content.

--- mount/server/models/dto/accounts.py
+++ mount/server/models/dto/accounts.py
@@ -1,15 +1,22 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class RoleEnum(str, Enum):
+    REGULAR = "REGULAR"
+    SUPER = "SUPER"
+    ADMIN = "ADMIN"
 
 
 class AccountDTO(BaseModel):
     username: str
     email: str
     password: str
-    role: str
+    role: RoleEnum
 
 
 class AccountUpdateDTO(BaseModel):
     username: str | None
     email: str | None
     password: str | None
-    role: str | None
+    role: RoleEnum | None
